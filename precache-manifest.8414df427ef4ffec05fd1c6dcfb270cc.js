e95812158181458fb96e5cc8d58a044f3d504345
paulus <paul_evu2006@yahoo.com>
2020-07-16 20:45:51 +0300

    Updates

---
+++ precache-manifest.8414df427ef4ffec05fd1c6dcfb270cc.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffb16c7c93d5b0f0758a560d00780d8",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "4b5aef4aa866946a3894",
+    "url": "/ecommerce/static/css/2.c1725561.chunk.css"
+  },
+  {
+    "revision": "c4c9b118fcfac046c997",
+    "url": "/ecommerce/static/css/main.a76f6099.chunk.css"
+  },
+  {
+    "revision": "4b5aef4aa866946a3894",
+    "url": "/ecommerce/static/js/2.9ca03bda.chunk.js"
+  },
+  {
+    "revision": "a90a0449d8ac6943f0cd62c0929c2a63",
+    "url": "/ecommerce/static/js/2.9ca03bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c9b118fcfac046c997",
+    "url": "/ecommerce/static/js/main.3ba8bf07.chunk.js"
+  },
+  {
+    "revision": "e424743e15616ced790c",
+    "url": "/ecommerce/static/js/runtime-main.e003cd19.js"
+  },
+  {
+    "revision": "d5fa56a9ccb00eb42b5142b95313fb45",
+    "url": "/ecommerce/static/media/10.d5fa56a9.png"
+  },
+  {
+    "revision": "3ab7d6d32d39ac4920af06c258f5bad8",
+    "url": "/ecommerce/static/media/11.3ab7d6d3.png"
+  },
+  {
+    "revision": "672cc9c9054f59e77ca5739b411bb5a6",
+    "url": "/ecommerce/static/media/12.672cc9c9.png"
+  },
+  {
+    "revision": "86e8d71777472b6dbe66b99cc52f7183",
+    "url": "/ecommerce/static/media/13.86e8d717.png"
+  },
+  {
+    "revision": "85c1fd6bb9cf3c6c5a8875d80529dc02",
+    "url": "/ecommerce/static/media/14.85c1fd6b.png"
+  },
+  {
+    "revision": "3e2946ec68d87016c91b8a69f2f9b70d",
+    "url": "/ecommerce/static/media/15.3e2946ec.png"
+  },
+  {
+    "revision": "dab7fdb60223175ffa912d5296e1227e",
+    "url": "/ecommerce/static/media/16.dab7fdb6.png"
+  },
+  {
+    "revision": "789ce8aa356489aca5a465f076064aae",
+    "url": "/ecommerce/static/media/17.789ce8aa.png"
+  },
+  {
+    "revision": "a184bd4d4e9acb91c7d82fd7d2cb31fb",
+    "url": "/ecommerce/static/media/1928.a184bd4d.png"
+  },
+  {
+    "revision": "ed48c7418dd30afb105b6205fc4b81d6",
+    "url": "/ecommerce/static/media/2000.ed48c741.png"
+  },
+  {
+    "revision": "9b6ff0649536b30cf25e6045cbf2fca2",
+    "url": "/ecommerce/static/media/2013.9b6ff064.png"
+  },
+  {
+    "revision": "c81aae3bab078e9c1f87f402d1b307f6",
+    "url": "/ecommerce/static/media/8.c81aae3b.png"
+  },
+  {
+    "revision": "cdb54fda3384592fc4300af53c753e36",
+    "url": "/ecommerce/static/media/80.cdb54fda.png"
+  },
+  {
+    "revision": "8fe64335b9746f54aaff99149cb7d1fe",
+    "url": "/ecommerce/static/media/9.8fe64335.png"
+  },
+  {
+    "revision": "2be77edde224244866db348dece6db21",
+    "url": "/ecommerce/static/media/90.2be77edd.png"
+  },
+  {
+    "revision": "908ea6d3f248135d268c24b699f4189b",
+    "url": "/ecommerce/static/media/Policy.908ea6d3.png"
+  },
+  {
+    "revision": "6fc658e77c40dfec04546b818977b969",
+    "url": "/ecommerce/static/media/bug.6fc658e7.png"
+  },
+  {
+    "revision": "c93160013bd02a72b8c85fc3c10491b2",
+    "url": "/ecommerce/static/media/contact.c9316001.png"
+  },
+  {
+    "revision": "2a39621dfa1af62bd4baf27c78f3e349",
+    "url": "/ecommerce/static/media/continued.2a39621d.png"
+  },
+  {
+    "revision": "1df2c9455ca29731fa86a3e9f4b79719",
+    "url": "/ecommerce/static/media/history.1df2c945.png"
+  },
+  {
+    "revision": "b4c3bb94d45fdf312a0687a9e68c634f",
+    "url": "/ecommerce/static/media/home-pic1.b4c3bb94.png"
+  },
+  {
+    "revision": "e71d8b1ce2cacb20b90d4c2b94d3d494",
+    "url": "/ecommerce/static/media/late90.e71d8b1c.png"
+  },
+  {
+    "revision": "1157dfe023626da0af766fc32914f2d7",
+    "url": "/ecommerce/static/media/man.1157dfe0.png"
+  },
+  {
+    "revision": "37bc77964698885b70e87c9d97b4c6bb",
+    "url": "/ecommerce/static/media/manbug.37bc7796.png"
+  },
+  {
+    "revision": "d99a495ec3a5c11d84473ca13c5a1aa0",
+    "url": "/ecommerce/static/media/play.d99a495e.png"
+  },
+  {
+    "revision": "41ae0fa316009ca8aaa077412ca234e5",
+    "url": "/ecommerce/static/media/premium.41ae0fa3.png"
+  },
+  {
+    "revision": "741d7f64c60274c469eca7619ff9d3ad",
+    "url": "/ecommerce/static/media/premium3.741d7f64.png"
+  },
+  {
+    "revision": "72762f35c0d8c3c6b385639fd30dc76a",
+    "url": "/ecommerce/static/media/sitemap.72762f35.png"
+  },
+  {
+    "revision": "1025e9b45d6357acbc98177df1aa94d9",
+    "url": "/ecommerce/static/media/store.1025e9b4.png"
+  },
+  {
+    "revision": "28a0b6709a4335521145daa02e471e62",
+    "url": "/ecommerce/static/media/woman.28a0b670.png"
+  },
+  {
+    "revision": "a69dfb1880e142d25e997241b0d08c3b",
+    "url": "/ecommerce/static/media/womanshop.a69dfb18.png"
+  }
+]);
