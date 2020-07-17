11f1889be6ca30d3f49697360cb0f316fa4d8c30
Evu Paulus <48010274+paulus1991@users.noreply.github.com>
2020-07-17 18:46:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Hashrouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { ProductProvider } from './context';
 
 ReactDOM.render(
 	<ProductProvider>
-		<Router>
+		<Router basename={process.env.PUBLIC_URL}>
 			<React.StrictMode>
 				<App />
 			</React.StrictMode>
